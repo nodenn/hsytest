a96ce77d1b6b0a211692ccbdf507eae5515e4677
nodenn <htndus96@naver.com>
2023-03-21 15:44:54 +0900

    Link

--- src/components/board/list/item.js
+++ src/components/board/list/item.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ImageDefault from "@/components/board/ImageDefault";
+import Link from "next/link";
 
 import { itemWrapper, itemContent } from "@/styles/board/Item.module.css";
 
 const Item = ({ item }) => {
   const { image, title, summary } = item;
   return (
-    <div className={itemWrapper}>
+    <Link href="/" className={itemWrapper}>
       <ImageDefault src={image} width={300} height={150} alt={title} />
       <div className={itemContent}>
         <h3>{title}</h3>
         <p>{summary}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
